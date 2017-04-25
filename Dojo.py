432d3a18f9e08341eeb4494d443f93dc68d65b76
SerryJohns <serembaj@gmail.com>
2017-04-25 18:24:45 +0300

    Assign Living Space

--- Dojo.py
+++ Dojo.py
@@ -28,7 +28,7 @@ class Dojo:
                     return False
             return True
 
-    def add_person(self, person_name, person_type, wants_accomodation = "N"):
+    def add_person(self, person_name, person_type, wants_accomodation="N"):
         if person_type == "Staff":
             return Staff(person_name, dojo.all_rooms)
         else:
@@ -43,6 +43,7 @@ dojo = Dojo()
 dojo.create_room("office", "Purple", "Black", "Brown")
 dojo.create_room("living space", "Yellow", "Orange", "Pink")
 dojo.add_person("Neil Armstrong", "Staff")
+dojo.add_person("Neil Armstrong", "Fellow", "Y")
 
 # print(dojo.all_rooms[0].room_name)
 # print(len(dojo.all_rooms))
--- Person.py
+++ Person.py
@@ -1,4 +1,5 @@
 from RandomRoomAllocator.Room import Office
+from RandomRoomAllocator.Room import LivingSpace
 from random import randint
 
 class Person:
@@ -6,7 +7,8 @@ class Person:
         self.person_name = person_name
 
     def allocate_office(self, person_name, rooms_list):
-        available_offices = [room for room in rooms_list if isinstance(room, Office) and (len(room.occupants) < 4)]
+        available_offices = [room for room in rooms_list
+                             if isinstance(room, Office) and (len(room.occupants) <= 6)]
         num_offices = len(available_offices)
         if num_offices > 0:
             office_to_assign = randint(0, num_offices)
@@ -22,7 +24,20 @@ class Staff(Person):
         self.allocate_office(self.person_name, rooms_list)
 
 
-class Fellow:
+class Fellow(Person):
     def __init__(self, person_name, opt_in, rooms_list):
         self.person_name = person_name
         self.allocate_office(self.person_name, rooms_list)
+        if opt_in:
+            self.allocate_living_space(self.person_name, rooms_list)
+
+    def allocate_living_space(self, person_name, rooms_list):
+        available_living_spaces = [room for room in rooms_list
+                                   if isinstance(room, LivingSpace) and (len(room.occupants) <= 4)]
+        num_spaces = len(available_living_spaces)
+        if num_spaces > 0:
+            space_to_assign = randint(0, num_spaces)
+            rooms_list[space_to_assign].occupants.append(self)
+            print("%s has been allocated the Living Space %s" % (person_name, rooms_list[space_to_assign].room_name))
+        else:
+            print("No Living Space to assign")
