a97f00901cad2efa8bc1fd5483eed3a528950348
SerryJohns <serembaj@gmail.com>
2017-04-25 13:43:33 +0300

    Added docopt

--- Dojo.py
+++ Dojo.py
@@ -1,5 +1,26 @@
+"""Greeter.
+
+Usage:
+ Dojo.py create_room <room_type> <room_name> ...
+ Dojo.py (-h | --help)
+
+Options:
+ -h --help     Show this screen.
+
+"""
 import docopt
+
 
 class Dojo:
     def __init__(self):
-        self.name = "Kenya Dojo"
+        self.dojo_name = "Kenya Dojo"
+        self.all_rooms = []
+        self.all_people = []
+
+
+    def create_room(self, room_type, room_name):
+        if room_type == "office":
+            return Office()
+
+if __name__ == '__main__':
+   arguments = docopt(__doc__)
