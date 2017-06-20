fcc1974d79d89ff3a89e3340bb714e38d9cf1484
John Paul Seremba <johnpaulseremba@Johns-MacBook-Pro.local>
2017-06-20 12:21:35 +0300

    Fix tests

--- tests/test_allocate_room.py
+++ tests/test_allocate_room.py
@@ -62,7 +62,9 @@ class TestAllocateRoom(unittest.TestCase):
     def test_allocations(self):  # pragma: no cover
         self.dojo.create_room("office", "Blue")
         self.dojo.add_person("Neil Jones", "Staff")
+        print("Into allocations tests")
         self.dojo.print_allocations("allocations_tests")
+        print("Out of allocations tests")
         my_file = open(os.path.join(os.path.dirname(__file__), "allocations_tests.txt")).read()
         self.assertTrue("BLUE" in my_file, msg="The room Blue should be printed to the file")
         self.assertTrue("Neil Jones" in my_file, msg="Neil Jones should be printed to the file")
